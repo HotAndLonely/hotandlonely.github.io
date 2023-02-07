39f8ebeb6ba80959434a000857fe84dbf8dfeb4b
xenonxss <akaxenonxss@gmail.com>
2023-02-07 19:58:43 +0100

    Commit emm

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import { Fade, Container, Grid, Paper, Stack, Typography } from '@mui/material';
+import { TypeAnimation } from 'react-type-animation';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+  return <>
+    <Fade in={true}>
+      <Container>
+        <TarjetaTerminal text={"Hello, I'm XSS"} />
+        <TarjetaTerminal text={"Hello, I'm XSS"} />
+        <TypeAnimation
+          strings={[
+            "I'm a Full Stack Developer",
+            "I Love Software Development",
+            "I Love All My Subscribers",
+          ]}
+          typeSpeed={150}
+          backSpeed={100}
+          loop /> 
+      </Container>
+    </Fade>
+  </>
 }
 
-export default App;
+function TarjetaTerminal(props) {
+  return <>
+    <Grid paddingTop={5}>
+      <Paper elevation={10}>
+        <Stack p={3}>
+          <Typography variant={'h4'} fontFamily={'Consolas'} color={'red'}>
+            ┌──(root💀portfolio)-[~]
+          </Typography>
+          <Typography variant={'h4'} fontFamily={'Consolas'} color={'red'}>
+            └─# {props.text}
+          </Typography>
+
+        </Stack>
+      </Paper >
+    </Grid>
+  </>
+}
