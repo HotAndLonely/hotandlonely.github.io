643e7b4fb9f59c8297f300aec95e5b92ee3e5f47
xenonxss <akaxenonxss@gmail.com>
2023-02-08 08:12:11 +0100

    Changes

--- src/App.js
+++ src/App.js
@@ -17,9 +17,15 @@ const darkTheme = createTheme({
 export default function App() {
 
   const options = [
-    { title: 'Github', link: 'https://github.com/HotAndLonely' },
-    { title: 'Discord', link: 'xenon#1272' },
-    { title: 'Email', link: 'akaxenonxss@gmail.com' }
+    { title: 'Github', link: 'https://github.com/HotAndLonely', cate: 'Social' },
+    { title: 'Discord', link: 'xenon#1272', cate: 'Social'},
+    { title: 'Email', link: 'akaxenonxss@gmail.com', cate: 'Social' },
+    { title: 'Lypus', link: 'gitlypus.c0m', cate: 'Group'},
+    { title: 'Roam', link: 'gitdeRoam', cate: 'Open source'},
+    { title: 'Geneva', link: 'gitdegeneva', cate: 'Open source'},
+    { title: 'Ethereum', link: 'wallete80230ru23r', cate: 'Finance'},
+    { title: 'Bitcoin', link: 'walleteBTC80230ru23r', cate: 'Finance'},
+    { title: 'Paypal', link: 'Paypal.menoseque', cate: 'Finance'}
   ]
 
   return <>
@@ -38,6 +44,7 @@ export default function App() {
                         options={options}
                         getOptionLabel={(option) => option.title}
                         sx={{ width: 200 }}
+                        groupBy={(options) => options.cate}
                         renderInput={(params) => <TextField id='tf' className='autocompleteStyle' variant='standard' {...params} />}
                         />]}
                       typingWords={['']}></TarjetaTerminal>
@@ -96,28 +103,3 @@ const TypingText = (props) => {
   );
 };
 
-class GetClientIp extends React.Component {
-
-  constructor(props) {
-    super(props)
-    this.state = {
-      ip: '',
-      location: '',
-    }
-  }
-
-  async componentDidMount() {
-    const res = await axios.get('https://geolocation-db.com/json/')
-    this.setState({
-      ip: res.data.IPv4,
-      location: res.data.country_name + ' ' + res.data.city
-    })
-    console.log(res)
-  }
-
-  render() {
-    return <>
-      {this.state.ip === '' ? <></> : <Typography variant='h6' paddingLeft={3} color={'#f5f5f5'}> {this.state.ip + ' From ' + this.state.location}</Typography>}
-    </>
-  }
-}
